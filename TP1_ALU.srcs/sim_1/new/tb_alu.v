eee570d818149544226f46796bccad3184b58b96
Juan Manuel Gil <juanmanuelgil99@gmail.com>
2022-09-14 19:21:53 -0300

    Improvement: code style

---
+++ TP1_ALU.srcs/sim_1/new/tb_alu.v
@@ -0,0 +1,108 @@
+`timescale 1ns / 1ps
+
+module tb_alu;
+    localparam BUS_OP_SIZE = 6;
+    localparam BUS_SIZE = 8;   
+    localparam BUS_BIT_ENABLE = 3;
+    localparam I_CLK = 1'b0;
+    localparam I_EN = 3'b000;
+    localparam I_DATA_A = 8'hFF; // 255 representado en hexa de N bits    
+    localparam I_DATA_B = 8'h02; // 2 representado en hexa de N bits    
+    localparam I_OPERATION = 6'h0; // operaci�n 0 representada en un hexa de M bits
+    localparam OP_ADD = 6'b100000;
+    localparam OP_SUB = 6'b100010;
+    localparam OP_AND = 6'b100100;
+    localparam OP_OR = 6'b100101;
+    localparam OP_XOR = 6'b100110;
+    localparam OP_SRA = 6'b000011;
+    localparam OP_SRL = 6'b000010;
+    localparam OP_NOR = 6'b100111;
+    //Inputs
+    reg i_clk;
+    reg[BUS_BIT_ENABLE -1 : 0] i_en;
+    reg[BUS_SIZE - 1 :0] i_data_a, i_data_b;
+    reg[BUS_OP_SIZE - 1 : 0] i_operation;
+    
+    //Outputs
+    wire[BUS_SIZE - 1 : 0] o_led;
+    wire o_carry_bit;
+    wire o_zero_bit;
+
+    // Verilog code for ALU
+    alu test_unit(
+            i_clk,
+            i_en,
+            i_data_a, i_data_b
+,  // ALU N-bit Inputs                 
+            i_operation
+,// ALU Operation
+            o_led, // ALU 8-bit Output
+            o_carry_bit
+, // Carry Out Flag,
+            o_zero_bit // Zero Out Flag
+        );
+    initial begin
+        i_clk = I_CLK;
+        i_en = I_EN;
+        i_data_a = I_DATA_A; // 255 representado en hexa de N bits
+        i_data_b = I_DATA_B; // 2 representado en hexa de N bits
+        i_operation = I_OPERATION; // operaci�n 0 representada en un hexa de M bits
+        #5
+        i_en[0] = 1;
+        #5
+        i_en[0] = 0;
+        i_en[1] = 1;
+        #5
+        i_en[1] = 0;
+        i_en[2] = 1;
+        #5
+        i_en[2] = 0;
+
+        i_operation = OP_ADD; // Addition
+        i_en[2] = 1;
+        #2
+        i_en[2] = 0;
+        #10;
+        i_operation = OP_SUB; // Subtraction
+        i_en[2] = 1;
+        #2
+        i_en[2] = 0;
+        #10;
+        i_operation = OP_AND; //  Logical and 
+        i_en[2] = 1;
+        #2
+        i_en[2] = 0;
+        #10;
+        i_operation = OP_OR; //  Logical or
+        i_en[2] = 1;
+        #2
+        i_en[2] = 0;
+        #10;
+        i_operation = OP_XOR; //  Logical xor 
+        i_en[2] = 1;
+        #2
+        i_en[2] = 0;
+        #10;
+        i_operation = OP_SRA; // SRA 
+        i_en[2] = 1;
+        #2
+        i_en[2] = 0;
+        #10;
+        i_operation = OP_SRL; // SRL
+        i_en[2] = 1;
+        #2
+        i_en[2] = 0;
+        #10;
+        i_operation = OP_NOR; // Logical nor
+        i_en[2] = 1;
+        #2
+        i_en[2] = 0;
+        #10
+        $finish;
+    end
+    
+    always begin
+        #1
+        i_clk = ~i_clk;
+    end
+endmodule
--- TP1_ALU.srcs/sources_1/new/tp1_alu.v
+++ TP1_ALU.srcs/sources_1/new/tp1_alu.v
@@ -3,40 +3,41 @@
 module alu#(
         parameter BUS_OP_SIZE = 6,
         parameter BUS_SIZE = 8,
-        parameter BUS_BIT_ENABLE = 3,
-        parameter OP_ADD = 6'b100000,
-        parameter OP_SUB = 6'b100010,
-        parameter OP_AND = 6'b100100,
-        parameter OP_OR = 6'b100101,
-        parameter OP_XOR = 6'b100110,
-        parameter OP_SRA = 6'b000011,
-        parameter OP_SRL = 6'b000010,
-        parameter OP_NOR = 6'b100111
+        parameter BUS_BIT_ENABLE = 3
     )(
-        input clk,
-        input [BUS_BIT_ENABLE - 1 : 0] in_en,
-        input [BUS_SIZE - 1 : 0] in_a, in_b,
-        input [BUS_OP_SIZE - 1 : 0] in_op,
-        output [BUS_SIZE - 1 : 0] out_led,
-        output out_carry,
-        output out_zero
+        input i_clk,
+        input [BUS_BIT_ENABLE - 1 : 0] i_en,
+        input [BUS_SIZE - 1 : 0] i_data_a, i_data_b,
+        input [BUS_OP_SIZE - 1 : 0] i_operation,
+        output [BUS_SIZE - 1 : 0] o_led,
+        output o_carry_bit,
+        output o_zero_bit
     );
+    localparam OP_ADD = 6'b100000;
+    localparam OP_SUB = 6'b100010;
+    localparam OP_AND = 6'b100100;
+    localparam OP_OR = 6'b100101;
+    localparam OP_XOR = 6'b100110;
+    localparam OP_SRA = 6'b000011;
+    localparam OP_SRL = 6'b000010;
+    localparam OP_NOR = 6'b100111;
+
     reg [BUS_SIZE - 1 : 0] data_a; 
     reg [BUS_SIZE - 1 : 0] data_b; 
-    reg [BUS_OP_SIZE - 1 : 0] data_op; 
+    reg [BUS_OP_SIZE - 1 : 0] data_operation; 
 
     reg[BUS_SIZE : 0] result; //tiene un bit extra para el carry
-    assign out_led = result; //7:0
-    assign out_carry = result[BUS_SIZE];
-    assign out_zero = ~|out_led;
+    assign o_led = result; //7:0
+    assign o_carry_bit = result[BUS_SIZE];
+    assign o_zero_bit = ~|o_led;
     
-    always @(posedge clk)  
+    always @(posedge i_clk)  
     begin
-        data_a = in_en[0] == 1 ? in_a : data_a;
-        data_b = in_en[1] == 1 ? in_b : data_b;
-        data_op = in_en[2] == 1 ? in_op : data_op;
+        data_a = i_en[0] == 1 ? i_data_a : data_a;
+        data_b = i_en[1] == 1 ? i_data_b : data_b;
+        data_operation = i_en[2] == 1 ? i_operation : data_operation;
             
-        case(data_op)
+        case(data_operation)
             OP_ADD: // Addition
                 result = {1'b0, data_a} + {1'b0, data_b}; 
             OP_SUB: // Subtraction
@@ -55,109 +56,5 @@ module alu#(
                 result = ~(data_a | data_b);
             default: result = data_a + data_b ; 
         endcase
-    end
-endmodule
-
-module tb_alu;
-    localparam BUS_OP_SIZE = 6;
-    localparam BUS_SIZE = 8;   
-    localparam BUS_BIT_ENABLE = 3;
-    localparam OP_ADD = 6'b100000;    
-    localparam OP_SUB = 6'b100010;    
-    localparam OP_AND = 6'b100100;    
-    localparam OP_OR = 6'b100101;    
-    localparam OP_XOR = 6'b100110;    
-    localparam OP_SRA = 6'b000011;    
-    localparam OP_SRL = 6'b000010;    
-    localparam OP_NOR = 6'b100111;
-    localparam IN_CLK = 1'b0;
-    localparam IN_EN = 3'b000;
-    localparam IN_A = 8'hFF; // 255 representado en hexa de N bits    
-    localparam IN_B = 8'h02; // 2 representado en hexa de N bits    
-    localparam IN_OP = 6'h0; // operaci�n 0 representada en un hexa de M bits
-
-    //Inputs
-    reg clk;
-    reg[BUS_BIT_ENABLE -1 : 0] in_en;
-    reg[BUS_SIZE - 1 :0] in_a, in_b;
-    reg[BUS_OP_SIZE - 1 : 0] in_op;
-    
-    //Outputs
-    wire[BUS_SIZE - 1 : 0] out_led;
-    wire out_carry;
-
-    // Verilog code for ALU
-    alu test_unit(
-            clk,
-            in_en,
-            in_a, in_b,  // ALU N-bit Inputs                 
-            in_op,// ALU Operation
-            out_led, // ALU 8-bit Output
-            out_carry, // Carry Out Flag,
-            out_zero // Zero Out Flag
-        );
-    initial begin
-        clk = IN_CLK;
-        in_en = IN_EN;
-        in_a = IN_A; // 139 representado en hexa de N bits
-        in_b = IN_B; // 2 representado en hexa de N bits
-        in_op = IN_OP; // operaci�n 0 representada en un hexa de M bits
-        #5
-        in_en[0] = 1;
-        #5
-        in_en[0] = 0;
-        in_en[1] = 1;
-        #5
-        in_en[1] = 0;
-        in_en[2] = 1;
-        #5
-        in_en[2] = 0;
-
-        in_op = OP_ADD; // Addition
-        in_en[2] = 1;
-        #2
-        in_en[2] = 0;
-        #10;
-        in_op = OP_SUB; // Subtraction
-        in_en[2] = 1;
-        #2
-        in_en[2] = 0;
-        #10;
-        in_op = OP_AND; //  Logical and 
-        in_en[2] = 1;
-        #2
-        in_en[2] = 0;
-        #10;
-        in_op = OP_OR; //  Logical or
-        in_en[2] = 1;
-        #2
-        in_en[2] = 0;
-        #10;
-        in_op = OP_XOR; //  Logical xor 
-        in_en[2] = 1;
-        #2
-        in_en[2] = 0;
-        #10;
-        in_op = OP_SRA; // SRA 
-        in_en[2] = 1;
-        #2
-        in_en[2] = 0;
-        #10;
-        in_op = OP_SRL; // SRL
-        in_en[2] = 1;
-        #2
-        in_en[2] = 0;
-        #10;
-        in_op = OP_NOR; // Logical nor
-        in_en[2] = 1;
-        #2
-        in_en[2] = 0;
-        #10
-        $finish;
-    end
-    
-    always begin
-        #1
-        clk = ~clk;
     end
 endmodule
