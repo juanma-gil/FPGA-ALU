04fe5744b351acff7f9477dcfdf78e350de542c6
Francisco Ciordia <gatupancho@gmail.com>
2022-12-11 16:21:10 -0300

    feat: Agregado el enable al latch

--- FPGA-ALU.srcs/sources_1/new/latch.v
+++ FPGA-ALU.srcs/sources_1/new/latch.v
@@ -23,24 +23,22 @@
 module latch#(
         parameter BUS_DATA = 8
     )(
-        input i_clock,
-        input reset,
+        input                       i_clock,
+        input                       i_reset,
+        input                       i_enable,
       
-        input [BUS_DATA - 1 : 0] i_data,
-        output [BUS_DATA - 1 : 0] o_data     
+        input   [BUS_DATA - 1 : 0]  i_data,
+        output  [BUS_DATA - 1 : 0]  o_data     
     );
-    reg [BUS_DATA - 1 : 0] data_reg;     
-    reg [BUS_DATA - 1 : 0] data_next; 
-
+    
+    reg [BUS_DATA - 1 : 0]  data_reg;     
+    
     always @(posedge i_clock)
-      if (reset)   
+      if (i_reset)   
          data_reg <= 0;
       else
-         data_reg <= data_next;
-
-    always@(*)
-        data_next = i_data;
-      
+        if(i_enable)
+            data_reg <= i_data;      
     
     assign o_data = data_reg;
 
